58e2d2df01e9121d3db57f9756b817483b42acbb
Krishrajput1107 <120461221+Krishrajput1107@users.noreply.github.com>
2024-02-17 11:33:55 +0530

    first commit

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     name='DiamondPricePrediction',
     version='0.0.1',
     author='Krish',
-    author_email='krish.naik@pw.live',
+    author_email='devgrade687@gmail.com',
     install_requires=get_requirements('requirements.txt'),
     packages=find_packages()
 
